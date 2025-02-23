d05a13c1c168d1912a2672b72ddb8b15f50deb2f
Yisaaaa <ryansanisit19@gmail.com>
2025-02-23 20:35:30 +0800

    Update grade_id references into assessment_id

--- app/db/init.sql
+++ app/db/init.sql
@@ -65,11 +65,11 @@ CREATE TABLE IF NOT EXISTS assessments (
 -- Create Scores Table
 CREATE TABLE IF NOT EXISTS scores (
     student_number TEXT NOT NULL,
-    grade_id INT NOT NULL,
+    assessment_id INT NOT NULL,
     score INT NOT NULL DEFAULT 0,  -- Default score is 0
-    PRIMARY KEY (student_number, grade_id),
+    PRIMARY KEY (student_number, assessment_id),
     FOREIGN KEY (student_number) REFERENCES students(student_number) ON DELETE CASCADE,
-    FOREIGN KEY (grade_id) REFERENCES assessments(grade_id) ON DELETE CASCADE
+    FOREIGN KEY (assessment_id) REFERENCES assessments(assessment_id) ON DELETE CASCADE
 );
 
 
