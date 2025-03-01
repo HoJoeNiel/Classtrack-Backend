0edc4c3a9e851893d99648b63776a070d0a965ae
Darren <edrichdarrensantuyo172030@gmail.com>
2025-03-01 10:29:26 +0800

    add CORS middleware

--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ from app.core.database import Database
 from app.db.repositories.grade_repository import score_trigger, student_score_trigger 
 from app.db.repositories.attendance_repository import attendance_record_trigger, students_attendance_trigger
 import asyncpg
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 
 @asynccontextmanager
@@ -26,6 +29,15 @@ async def lifespan(app: FastAPI):
 	await Database.close_all()
 
 app = FastAPI(lifespan=lifespan)
+
+# TO CONNECT OUR BACKEND TO FRONTEND
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins = ["http://localhost:3000"], # React FrontEnd URL
+	allow_credentials = True, 	
+	allow_methods = ["*"], # Allow all HTTP request (GET, POST, PUT...)
+	allow_headers = ["*"]  # Allow all headers like Authorization
+)
 
 
 # Error handler middlewares
